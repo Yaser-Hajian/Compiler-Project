ec57a717ed88147392e4af3035be3672bc88f3e8
Yaser <mohammadyaser1379@gmail.com>
2021-11-11 00:29:47 +0330

    add highlighter and it writes tokens in html file which has a style

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
+    public static void highlight(ArrayList<Scanner.Token> arrayList , FileWriter fileWriter) throws IOException{
+        for (int i = 0; i < arrayList.size(); i++) {
+            int lineNumber = i+1;
+            Scanner.Token token = arrayList.get(i);
+            fileWriter.write("<p class=\"" +token.type+ "\">" +"<span class=\"count\">"+lineNumber + "- "+"</span>"+ token.value+"</p>");
+        }
+        fileWriter.write("</body>");
+        fileWriter.write("</html>");
+        fileWriter.flush();
+        fileWriter.close();
+    }
     public static void main(String[] args) throws IOException {
         ArrayList<Scanner.Token> tokens = new ArrayList<>();
+        FileWriter fileWriter = new FileWriter("src/Output/output.html" , true);
         try {
             Scanner scanner = new Scanner(new FileReader("src/input.cool"));
             while (true) {
@@ -16,8 +29,11 @@ public class Main {
                 if (!currentToken.type.startsWith("Error")) {
                     tokens.add(currentToken);
                 }
+                System.out.println("---------------------");
+                System.out.println("size:"+tokens.size());
                 System.out.println(currentToken.type + ": "+ currentToken.value);
             }
+            highlight(tokens,fileWriter);
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
