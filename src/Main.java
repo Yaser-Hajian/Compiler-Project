e9cd3c60e5f79af40d8f90bc2855fa6e23fb5b5d
Yaser <mohammadyaser1379@gmail.com>
2021-11-21 10:20:14 +0330

    all tokens in one line will print in single line

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,45 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void highlight(ArrayList<Scanner.Token> arrayList , FileWriter fileWriter) throws IOException{
+//        for (int i = 0; i < arrayList.size(); i++) {
+//            int lineNumber = i+1;
+//            Scanner.Token token = arrayList.get(i);
+//            fileWriter.write("<p class=\"" +token.type+ "\">" +"<span class=\"count\">"+lineNumber + "- "+"</span>"+ token.value+"</p>");
+//        }
+
+
+        Map<Integer , ArrayList<Scanner.Token>  >map_token_to_line_number=new HashMap<>();
+
         for (int i = 0; i < arrayList.size(); i++) {
-            int lineNumber = i+1;
             Scanner.Token token = arrayList.get(i);
-            fileWriter.write("<p class=\"" +token.type+ "\">" +"<span class=\"count\">"+lineNumber + "- "+"</span>"+ token.value+"</p>");
+            int line_number = token.lineNumber;
+
+            if (!map_token_to_line_number.containsKey(line_number)){
+                map_token_to_line_number.put(line_number , new ArrayList<Scanner.Token>());
+            }
+            map_token_to_line_number.get(line_number).add(token);
+
+
+        }
+        for (int i : map_token_to_line_number.keySet()) {
+            ArrayList<Scanner.Token> tokens = map_token_to_line_number.get(i);
+            String writed = "<p>";
+            int lineNumber = i+1;
+            System.out.println(i);
+            writed += "<span class=\"count\">"+lineNumber + "- </span> ";
+            for (int j = 0; j < tokens.size(); j++) {
+                Scanner.Token token = tokens.get(j);
+                writed += "<span class=\" " +token.type+  "\">"+ token.value+"</span>";
+                System.out.println(token.value);
+            }
+            writed += "</p>";
+            fileWriter.write(writed);
+
         }
         fileWriter.write("</body>");
         fileWriter.write("</html>");
